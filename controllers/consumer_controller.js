45ce44c5b45402bfd72205ebeafce7083c322e01
Tom Brander <tom.e.brander@gmail.com>
2020-11-13 12:17:02 +0100

    Updated consumer controller

--- controllers/consumer_controller.js
+++ controllers/consumer_controller.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose'),
-    consumption = mongoose.model('consumption');
+    consumer = mongoose.model('consumer');
 
 exports.updateConsumption = function(req, res) {
-    
+    consumer.findOneAndUpdate({_id: req.params.ID}, req.body, {new: true}, function(err, consumer) {
+        if (err)
+          res.send(err);
+        res.json(consumer);
+      });
 }
