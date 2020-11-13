581356a61a176ddf4d49dd9131d2524ea5faf51d
Tom Brander <tom.e.brander@gmail.com>
2020-11-13 14:20:33 +0100

    Gaussian distribution implemented and used in wind and consumption predictions

--- Simulator/simulator.js
+++ Simulator/simulator.js
@@ -1,14 +1,41 @@
+function gaussian(mean, stdev) {
+    var y2;
+    var use_last = false;
+    return function() {
+        var y1;
+        if(use_last) {
+           y1 = y2;
+           use_last = false;
+        }
+        else {
+            var x1, x2, w;
+            do {
+                 x1 = 2.0 * Math.random() - 1.0;
+                 x2 = 2.0 * Math.random() - 1.0;
+                 w  = x1 * x1 + x2 * x2;               
+            } while( w >= 1.0);
+            w = Math.sqrt((-2.0 * Math.log(w))/w);
+            y1 = x1 * w;
+            y2 = x2 * w;
+            use_last = true;
+       }
 
-
+       var retval = mean + stdev * y1;
+       if(retval > 0) 
+           return retval;
+       return -retval;
+   }
+}
 function simulateWind(currentWind)
 {
-    wind = currentWind;
+    wind = gaussian(currentWind, 1)
     return wind;
 }
 
 function simulateConsumption(currentConsumption)
 {
-    consumption = currentConsumption;
+
+    consumption = gaussian(currentConsumption)
     return consumption;
 }
 
@@ -21,15 +48,18 @@ function calcPrice(wind, currentConsumption)
 
 function runSim()
 {
-    wind = Math.random(0,10); // m/s
-    consumption = Math.random(0,800) // kwh
+    wind = gaussian(3.6, 1); // m/s
+    consumption = gaussian(70, 1) // kwh
     price = -1;
     while(true)
     {
         wind = simulateWind(wind);
         consumption =  simulateConsumption(consumption);
+        console.log(wind);
+        console.log(consumption);
 
         price = calcPrice(wind, consumption);
+        
         //PUSH PRICE TO DB
     }
 }
