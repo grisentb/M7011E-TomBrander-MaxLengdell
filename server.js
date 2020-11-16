8f03e2268a53950f65a15c31919ba8c153b3e32e
max <maxlen-7@student.ltu.se>
2020-11-16 19:00:36 +0100

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ Consumer = require('./models/models'),
 bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/M7011E/database');
+mongoose.connect('mongodb://localhost/M7011E');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
