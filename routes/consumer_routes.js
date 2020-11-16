d7573d463830db3fc1563701e0c84591b4856b7c
max <maxlen-7@student.ltu.se>
2020-11-16 18:50:46 +0100

    bug fix

--- routes/consumer_routes.js
+++ routes/consumer_routes.js
@@ -2,7 +2,7 @@ module.exports = function(app){
     var consumer = require('../controllers/consumer_controller');
 
     app.route('/consumer/consumption')
-        .get(consumer.consumption)
+        .get(consumer.listAllClients)
         .put(consumer.updateConsumption)
         .post(consumer.createHousehold)
 }
