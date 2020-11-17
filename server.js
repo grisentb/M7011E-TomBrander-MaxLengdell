ee94b48754fa5b8c1f83209be1546300f482b40f
max <maxlen-7@student.ltu.se>
2020-11-17 16:21:33 +0100

    JEST test suite installed

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var express = require('express'), 
 app = express(), 
 port = process.env.PORT || 3000,
-mongoose = require('mongoose'),
 bodyParser = require('body-parser');
 schemas = require('./models/models');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/M7011E');
-
+var mongoose;
+connectDatabase();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -23,3 +20,10 @@ simulator.runSim();
 app.listen(port);
 
 console.log('restful api server started on: ' + port);
+
+function connectDatabase(){
+    mongoose = require('mongoose');
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/M7011E');
+}
+module.exports = connectDatabase;
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,16 @@
 const sum = require('./function');
 const sim = require('./../Simulator/simulator');
+const db = require('./../Simulator/databaseConnection');
+
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
 });
 
 test("Verifying gaussian is truly random", () => {
-    expect(sim.gaussian()).toBe
-})
+    var temp = sim.gaussian(100,100);
+    expect(sim.gaussian(100,100)).not.toEqual(temp);
+});
+
+test("Wind and consumption affect on price", () => {
+    expect(1).toEqual(1);
+});
