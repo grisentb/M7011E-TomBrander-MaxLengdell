45b2bb305c2b567e5db237e2b49ec4a70bdf795a
max <maxlen-7@student.ltu.se>
2020-11-26 14:58:32 +0100

    register fix

--- Webserver/client/app_react/src/validation/register_validation.js
+++ Webserver/client/app_react/src/validation/register_validation.js
@@ -2,34 +2,39 @@ const Validator = require("validator");
 const isEmpty = require("is-empty");
 const { default: validator } = require("validator");
 
-module.exports = function validateRegisterInput(data) {
+module.exports = function validateRegisterInput(name, email, password, password2) {
     let errors = {};
-
+    console.log(name, " : ", email);
+    console.log(password, " : ", password2);
+    name = name.value;
+    email = email.value;
+    password = password.value;
+    password2 = password2.value;
     //Set empty fields to empty strings.
-    data.name = !isEmpty(data.name) ? data.name : "";
-    data.email = !isEmpty(data.email) ? data.email : "";
-    data.password = !isEmpty(data.password) ? data.password : "";
-    data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+    name = !isEmpty(name) ? name : "";
+    email = !isEmpty(email) ? email : "";
+    password = !isEmpty(password) ? password : "";
+    password2 = !isEmpty(password2) ? password2 : "";
 
     //Validate name
-    if (Validator.isEmpty(data.name)) {
+    if (Validator.isEmpty(name)) {
         errors.name = "Name field is required to register";
     }
     //Validate email
-    if (Validator.isEmpty(data.email)) {
+    if (Validator.isEmpty(email)) {
         errors.email = "Email field is required to register";
     }
     // Password checks
-    if (Validator.isEmpty(data.password)) {
+    if (Validator.isEmpty(password)) {
         errors.password = "Password field is required to register";
     }
-    if (Validator.isEmpty(data.password2)) {
+    if (Validator.isEmpty(password2)) {
         errors.password2 = "Confirm password field is required to register";
     }
-    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    if (!Validator.isLength(password, { min: 6, max: 30 })) {
         errors.password = "Password must be at least 6 characters to register";
     }
-    if (!Validator.equals(data.password, data.password2)) {
+    if (!Validator.equals(password, password2)) {
         errors.password2 = "Passwords must match";
     }
     return {
