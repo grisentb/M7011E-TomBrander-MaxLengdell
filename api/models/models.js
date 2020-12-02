4df0c4c17b44514489af1dce51db8f49b42d0678
max <maxlen-7@student.ltu.se>
2020-12-02 14:05:04 +0100

    image model

--- api/models/models.js
+++ api/models/models.js
@@ -75,8 +75,7 @@ var userSchema = new Schema({
         default: Date.now
     },
     image: {
-        data: Buffer,
-        contentType: String
+        type: String
     }
 });
 module.exports = mongoose.model('consumer', consumerSchema);
